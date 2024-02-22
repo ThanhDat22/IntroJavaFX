509d53f5f3708487ca3bd43dbb89d6ab5d00a0c0
ThanhDat22 <dat.nguyen2284@gmail.com>
2024-02-22 13:01:53 -0600

    Add ShowCircle

--- src/main/java/com/example/introjavafx/ShowCircle.java
+++ src/main/java/com/example/introjavafx/ShowCircle.java
@@ -1,4 +1,37 @@
 package com.example.introjavafx;
 
-public class ShowCircle {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ShowCircle extends Application {
+    /**Override the start method in the Application class
+     * @param primaryStage: the stage
+     * @throws IOException
+     */
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        // Creates a circle and set its properties
+        Circle circle = new Circle();
+        circle.setCenterX(100);
+        circle.setCenterY(100);
+        circle.setRadius(50);
+        circle.setStroke(Color.BLACK);
+        circle.setFill(Color.WHITE);
+
+        // Creates a pane to hold the circle
+        Pane pane = new Pane();
+        pane.getChildren().add(circle);
+
+        // Creates a scene and place it in the stage
+        Scene scene = new Scene(pane, 200, 200);
+        primaryStage.setTitle("ShowCircle"); // Set the stage tile
+        primaryStage.setScene(scene); // place the scene in the stage
+        primaryStage.show(); // Display the stage
+    }
 }
